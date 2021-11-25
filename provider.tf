05573bd0e91900ab371f4d4d3a72a2d94917e2f4
vishnu82577 <94585993+vishnu82577@users.noreply.github.com>
2021-11-25 22:02:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,7 @@
 provider "azurerm" {
   features {}
   version         = "=2.46.0"
-  subscription_id = "0bfbc3da-d0c9-4f27-aace-655fdf0480d8"
-  client_id       = "379f941f-e6e7-40b1-bb9c-28cb024c0acb"
-  client_secret   = "lO27Q~oY2dUaMX6K6PjobqRwZv3PXAxqJObdt"
-  tenant_id       = "91e3665f-39b5-4ab0-be1b-ee2762993f70"
-}
+  }
 
 terraform {
   backend "azurerm" {
